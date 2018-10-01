1084c1e87967ef31f10f81eaa199d956619f7b7b
Jonathan Laptop <indiajonathan@tamu.edu>
2018-10-01 13:28:18 -0500

    Can now take in code in text form and return results

--- codeReader.js
+++ codeReader.js
@@ -1,5 +1,6 @@
 
-const {NodeVM} = require('vm2');
+//const {NodeVM, VMScript} = require('vm2');
+const {NodeVM, VMScript} = require('vm2');
 const readline = require('readline');
 const vm = new NodeVM({
     console: 'inherit',
@@ -19,32 +20,62 @@ const vm = new NodeVM({
 
 class codeReader{
     constructor(P1Code,P2Code) {
-        this.P1Code = P1Code;
-        this.P2Code = P2Code;
+        this.P1CodeString = 'module.exports = function(boardState) { '+ P1Code + ' }';
+        this.P2CodeString = 'module.exports = function(boardState) { '+ P2Code + ' }';
+        this.P1CodeCompiled = {};
+        this.P2CodeCompiled = {};
     }
 
-    compileCheckCode(codeArg){
+    saveCode(codeArg){
         try {
-            let script = new VMScript("Math.random()").compile();
+            this.P1Code = new VMScript(this.P1CodeString);
         } catch (err) {
             console.error('Failed to compile script.', err);
         }
     }
 
-    runP1CodeTurn(){
+    runP1CodeTurn(boardState){
+        let p1ReturnVal = -1;
         try{
-        let functionInSandboxP1 = vm.run(this.P1Code);
-        var p1ReturnVal = functionInSandboxP1(boardState);
+            //let tempString = 'module.exports = function(boardState) { '+ this.P1Code + ' }';
+            let p1functionInSandbox = vm.run(this.P1Code);
+            p1ReturnVal = p1functionInSandbox(boardState);
         } catch (err) {
+            //Return Debugging Errors here
             console.error('Failed to execute script.', err);
         }
         return p1ReturnVal;
     }
 
+    runP2CodeTurn(boardState){
+        let p2ReturnVal = -1;
+        try{
+        let p2ReturnVal = vm.run(this.P1Code);
+        //var p2ReturnVal = functionInSandboxP2(boardState);
+        } catch (err) {
+            console.error('Failed to execute script.', err);
+        }
+        return p2ReturnVal;
+    }
+
     checkCodeThenRun(){
-        this.compileCheckCode(this.P1Code);
-        for (let index = 0; index < 3; index++) {
-            console.log(this.runP1CodeTurn);
+        this.saveCode(this.P1Code);
+        //DECLARE INITIAL BOARD STATE HERE
+
+        let gameWon = false;
+        while (!gameWon) {
+
+            //Run p1 turn
+            let p1Selection = this.runP1CodeTurn("temp");
+            console.log("Player One Wants to put a checker in " + p1Selection);
+            //Modify Board State Here
+
+            //Run p2 Turn
+            let p2Selection = this.runP2CodeTurn("temp2");
+            console.log("Player Two Wants to put a checker in " + p2Selection);
+
+            //Check Win Condition Here
+            gameWon = true;
         }
     }
     
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const CODEREADER = require('./codeReader.js');
+const {codeReader} = require('./codeReader.js');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -13,15 +13,5 @@ express()
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
-
-  let codeRunner = new CODEREADER("temp","Temp");
-
-  // let p1Code = "console.log('test'); let temp = 2; console.log(boardState); return 2;"
-  // let p2Code = "return 1";
-  // let boardState = "this is an example object";
-  // let functionInSandboxP1 = vm.run("module.exports = function(boardState) { "+p1Code+" }");
-  // let functionInSandboxP2 = vm.run("module.exports = function(boardState) { "+p2Code+" }");
-  // var p1ReturnVal = functionInSandboxP1(boardState);
-  // //Modify Board State based on input from p1 here
-  // var p2ReturnVal = functionInSandboxP2(boardState);
-  // console.log(p1ReturnVal);
+  let codeRunner = new codeReader("return 1;","return 2;");
+  codeRunner.checkCodeThenRun();
