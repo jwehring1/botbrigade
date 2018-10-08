d61353d28c62901f1a361c4109c6f6dbf087b5d0
jwehring1 <jwehring@tamu.edu>
2018-10-08 11:46:48 -0500

    Trying to find recursion problem

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ for (var i = 0; i < gameBoard.length; i++){
   gameBoard[i].fill(0);
 }
 
-function PlaceChecker(col,player){
+function PlaceChecker(col,player,temp){
+  if(gameBoard[0][col]!=0){
+	  return -1;
+  }
   for(var i = 0; i<6; i++){
     if(gameBoard[i][col]!=0){
       break;
     }
   }
-  gameBoard[i-1][col] = player;
+  if(!temp) {
+	gameBoard[i-1][col] = player;
+  }
   return i-1;
 }
-
 
 function checkAdj(a,b,c,d) {
     // Check first cell non-zero and all cells match
@@ -44,7 +48,71 @@ function checkWinner(placedRow,placedColumn) {
                 return gameBoard[row][col];
 
     return 0;
-}/*
+}
+
+function solveV2(givenHeight, player)
+{
+	for(i=0; i< givenHeight;i++)
+	{
+		console.log("givenHeight: "+givenHeight);
+		solveV2(givenHeight-1,1);
+	}
+}
+
+function solve(givenHeight, player) {
+	Height = givenHeight;
+	let pos = -1;
+	if(Height > 0) {
+		console.log("Height: "+Height);
+		min = 99999999;//Not my turn
+		if(player == 1) {
+			for(i = 0; i < 7; i++) {
+				let tempScore = 0;
+				let row = PlaceChecker(i,1, true);
+				console.log("row: "+row);
+				if(row==-1) {
+					continue;
+				}
+				if(checkWinner==1) {
+					tempScore = tempScore - (Height * 4);
+				}
+				let max = solve(Height-1,2);
+				console.log("tempScore + max: "+tempScore + max);
+				if(min>tempScore+max) {
+					min = tempScore + max;
+					pos = i;
+				}
+			}
+			return min;
+		}
+		max = -99999999;//AI Turn
+		if(player == 2) {
+			for(i = 0; i < 7; i++) {
+				let tempScore = 0;
+				let row = PlaceChecker(i,2, true);
+				if(row==-1) {
+					continue;
+				}
+				if(checkWinner==2) {
+					tempScore = tempScore + (Height * 3);
+				}
+				let min = solve(Height-1,1);
+				if(tempScore+min>max) {
+					max=tempScore+min;
+					pos=i;
+				}
+			}
+			if(Height == 10) {
+				return pos;
+			}
+			else {
+				return max;
+			}
+		}
+	}
+	return 0;
+}
+/*
 PlaceChecker(0,2);
 PlaceChecker(0,1);
 PlaceChecker(0,2);
@@ -54,28 +122,29 @@ PlaceChecker(1,2);
 PlaceChecker(1,2);
 PlaceChecker(2,1);
 PlaceChecker(2,2);
-PlaceChecker(3,2);*/
+PlaceChecker(3,2);
 
 PlaceChecker(0,2);
-PlaceChecker(1,1);
-PlaceChecker(1,2);
-PlaceChecker(2,1);
-PlaceChecker(2,2);
-PlaceChecker(2,2);
-PlaceChecker(3,2);
+PlaceChecker(0,1);
+PlaceChecker(0,2);
+PlaceChecker(0,1);
+PlaceChecker(0,2);
+PlaceChecker(0,2);
+console.log(PlaceChecker(0,2));
 PlaceChecker(3,1);
-PlaceChecker(3,2);
+PlaceChecker(3,1);
 PlaceChecker(4,1);
 PlaceChecker(3,2);
-PlaceChecker(4,2);
-PlaceChecker(4,1);
-PlaceChecker(4,2);
-PlaceChecker(5,1);
-PlaceChecker(4,2);
+PlaceChecker(4,1);*/
+console.log(gameBoard);
+//console.log("Pos: "+solve(10,1));
+solveV2(10,1);
 
-console.log(checkWinner(3,0));
+console.log("Winner: "+checkWinner(3,0));
 
 console.log(gameBoard);
+
+
 
 const cool = require('cool-ascii-faces')
 const express = require('express')
