08e089ae13d15e80514fa7825e8aab8d01cb7d41
jwehring1 <jwehring@tamu.edu>
2018-10-23 18:27:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,73 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
 
-const cool = require('cool-ascii-faces')
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var indexRouter = require('.application/routes/index');
+var tutorial = require('.application/routes/tutorial');
+var input_ai = require('.application/routes/input_ai');
 const {AIBattle,randomAI,alwaysPlaceAt1,codeReader,roundRobin,readTextFile,printAI} = require('./codeReader.js');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+
+// for parsing application/json
+app.use(bodyParser.json()); 
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.use(upload.array()); 
+app.use(express.static('public'));
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+app.use('/tutorial', tutorial);
+app.use('/input_ai', input_ai);
+
+//Functionality for textbox
+app.post('/input', function(req, res){
+
+  let challenger = new codeReader(req.body.code2,"PlayerCode");
+	let battleReport = roundRobin(challenger,1,1);
+	res.redirect('/input_ai');
+
+
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
 
 
 
